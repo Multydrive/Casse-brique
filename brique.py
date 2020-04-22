281751d7530a4c62886ade76c0d4501df3bec25f
Jérémy MORTIER <jeremy.mortier@std.heh.be>
2020-04-22 16:45:46 +0200

    Brique random

--- brique.py
+++ brique.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 class Brick(pygame.sprite.Sprite):
     def __init__(self, position: tuple):
         pygame.sprite.Sprite.__init__(self)
@@ -14,20 +15,28 @@ class Wall(pygame.sprite.Group):
         self.pos_x = 0
         self.pos_y = 20
 
-        # 640 / 16 = Cantidad de ladrillos por fila
-
-        # Construcción de muro
-        # Cantidad de ladrillos múltiplos de 16
-
         for x in range(0, 65):
 
             if self.pos_x >= 1040:
                 self.pos_x = 0
                 self.pos_y = self.pos_y + brick.rect.height
 
-            brick = Brick((self.pos_x, self.pos_y))
+            choix = random.randint(0,3)
+            if choix == 0:
+                brick = Brick((self.pos_x, self.pos_y))
+                brick.image = pygame.image.load('assets/bleu.jpg')
 
-            # Agregar Sprite al grupo
+            elif choix == 1:
+                brick = Brick((self.pos_x, self.pos_y))
+                brick.image = pygame.image.load('assets/vert.jpg')
+
+            elif choix == 2:
+                brick = Brick((self.pos_x, self.pos_y))
+                brick.image = pygame.image.load('assets/rouge.jpg')
+
+            elif choix == 3:
+                brick = Brick((self.pos_x, self.pos_y))
+                brick.image = pygame.image.load('assets/us.jpg')
 
             self.add(brick)
             self.pos_x = self.pos_x + brick.rect.width
